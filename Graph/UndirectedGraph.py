6d21f1df2c1c718a6cc23dd4ac03879b71db2a69
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-01-28 21:30:53 -0700

    implemented bfs

--- Graph/UndirectedGraph.py
+++ Graph/UndirectedGraph.py
@@ -35,11 +35,34 @@ class UndirectedGraph:
 			print(key, ":", value_list)
 		print()
 
+	def BFS(self, start_vertex, end_vertex):
+		queue = [start_vertex]
+		visited = [start_vertex]
+
+		while(len(queue) != 0):
+			popped = queue.pop(0)
+			if popped == end_vertex:
+				return True
+
+			for adjacent_edge in self.graph[popped]:
+				if adjacent_edge not in visited:
+					visited.append(adjacent_edge)
+					queue.append(adjacent_edge)
+
+		return False
+
 graph = UndirectedGraph()
 
 graph.add_edge(0, 1)
 graph.add_edge(0, 2)
 graph.add_edge(1, 2)
 graph.add_edge(2, 3)
+graph.add_edge(4, 5)
+graph.add_edge(5, 6)
 
 graph.print_graph()
+
+isPath = graph.BFS(0, 5)
+print(isPath)
+isPath = graph.BFS(0, 3)
+print(isPath)
